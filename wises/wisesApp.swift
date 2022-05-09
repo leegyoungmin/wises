04ec4d8be99bf90c33b09c10def8af2a294761b9
이경민 <cow970814@naver.com>
2022-05-09 16:52:59 +0900

    Initial Commit

---
+++ wises/wisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  wisesApp.swift
+//  wises
+//
+//  Created by 이경민 on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct wisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
